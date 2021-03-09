8c1c9ad3c5f53066ef8e899265e1eded10341237
Josh Mabry <jmabry@phase2technology>
2021-03-08 18:47:00 -0800

    refactor story to use controls

--- src/components/my-component/my-component.stories.ts
+++ src/components/my-component/my-component.stories.ts
@@ -1,12 +1,22 @@
-import { text } from '@storybook/addon-knobs';
-
 export default {
   title: 'Example/Demo',
+  component: 'my-component',
+  argTypes: {
+    firstName: {
+      control: 'text',
+      defaultValue: 'Stencil',
+    },
+    middleName: {
+      control: 'text',
+      defaultValue: 'Storybook',
+    },
+    lastName: {
+      control: 'text',
+      defaultValue: 'Starter',
+    },
+  },
 };
 
-export const MyComponent = () => {
-  const firstName = text('First name', 'Stencil');
-  const middleName = text('Middle name', 'Storybook');
-  const lastName = text('Last name', 'Starter');
+export const MyComponent = ({ firstName, middleName, lastName }) => {
   return `<my-component first="${firstName}" middle="${middleName}" last="${lastName}"></my-component>`;
 };
