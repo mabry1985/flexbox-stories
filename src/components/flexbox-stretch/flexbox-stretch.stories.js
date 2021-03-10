6882997c6c854b326e19e7eaf9c4fb527c0eb234
Josh Mabry <jmabry@phase2technology>
2021-03-09 16:45:17 -0800

    add codeblock component and render css

--- src/components/flexbox-stretch/flexbox-stretch.stories.js
+++ src/components/flexbox-stretch/flexbox-stretch.stories.js
@@ -6,5 +6,25 @@ export default {
 export const FlexboxStretch = () => {
   return `
             <flexbox-stretch></flexbox-stretch>
+            <code-block>
+            .container {
+                 display: flex;
+            }
+
+            .flex-item {
+                background: #42E629;
+                flex-grow: 1;
+                height: 100px;
+            }
+
+            .flex-item:nth-child(odd) {
+                background: #36A7E3;
+                flex-grow: 2;
+            }
+
+            .flex-item + .flex-item {
+                margin-left: 2%;
+            }
+            </code-block>
         `;
 };
